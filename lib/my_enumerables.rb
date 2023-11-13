e8f90e66abe435c07ee72737e25fe84c5790db01
„kurniker“ <kridijaw@gmail.com>
2023-11-13 20:53:15 +0100

    Feat: my_all?

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,22 +1,11 @@
 # frozen_string_literal: true
 
-# The following table has the methods you will need to implement along with
-# their associated specs, as outlined in the project instructions. The methods
-# in the Enumerable module only need to work with Arrays, but you can
-# challenge yourself and try to make them work for Hashes as well.
-
-# Method	                      Spec File	Notes
-# Enumerable#my_select	        spec/my_select_spec.rb
 # Enumerable#my_all?	          spec/my_all_spec.rb
 # Enumerable#my_any?	          spec/my_any_spec.rb
 # Enumerable#my_none?	          spec/my_none_spec.rb
 # Enumerable#my_count	          spec/my_count_spec.rb
 # Enumerable#my_map	            spec/my_map_spec.rb
 # Enumerable#my_inject	        spec/my_inject_spec.rb
-
-# Testing
-# bundle exec rspec spec/my_each_spec.rb
-# ...
 
 # Collection of iteration methods, inspired by Ruby's Enumerable module
 module Enumerable
@@ -32,11 +21,19 @@ module Enumerable
   # Returns an array containing elements selected by the block.
   # With a block given, calls the block with successive elements.
   def my_select(&block)
-    selected_elments = []
+    selected_elements = []
     my_each do |i|
-      selected_elments << i if block.call(i)
+      selected_elements << i if block.call(i)
     end
-    selected_elments
+    selected_elements
+  end
+
+  # Returns whether every element meets a given criterion.
+  def my_all?(&block)
+    my_each do |i|
+      return false if block.call(i) == false
+    end
+    true
   end
 end
 
@@ -56,14 +53,3 @@ class Array
 end
 
 enumerable = [1, 1, 2, 3, 5, 8, 13, 21, 34]
-
-# yield vs &block
-# def transaction_statement
-#  @transactions.each do |transaction|
-#    yield transaction # You just yield the transaction amount.
-#   end
-# end
-# ===
-#  def my_each(&block)
-#   @test_arr.each(&block)
-#  end
