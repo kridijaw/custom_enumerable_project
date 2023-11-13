53d7eb5daa6cfd7e27f0794b66cbcfe970999f64
„kurniker“ <kridijaw@gmail.com>
2023-11-13 19:17:06 +0100

    Feat: my_select

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -6,7 +6,6 @@
 # challenge yourself and try to make them work for Hashes as well.
 
 # Method	                      Spec File	Notes
-# Enumerable#my_each_with_index	spec/my_each_with_index_spec.rb
 # Enumerable#my_select	        spec/my_select_spec.rb
 # Enumerable#my_all?	          spec/my_all_spec.rb
 # Enumerable#my_any?	          spec/my_any_spec.rb
@@ -19,10 +18,30 @@
 # bundle exec rspec spec/my_each_spec.rb
 # ...
 
-# define all methods, but #my_each here
+# Collection of iteration methods, inspired by Ruby's Enumerable module
 module Enumerable
+  # Iterates the given block for each element with an index.
+  def my_each_with_index
+    index = 0
+    my_each do |i|
+      yield(i, index)
+      index += 1
+    end
+  end
+
+  # Returns an array containing elements selected by the block.
+  # With a block given, calls the block with successive elements.
+  def my_select(&block)
+    selected_elments = []
+    my_each do |i|
+      selected_elments << i if block.call(i)
+    end
+    selected_elments
+  end
 end
 
+# inspired by Ruby's Array#each
+# Methods of the enumerable module above will have access to this method
 class Array
   # Calls the given block once for each element in self,
   # passing that element as a parameter. Returns the array itself.
@@ -35,6 +54,8 @@ class Array
     self
   end
 end
+
+enumerable = [1, 1, 2, 3, 5, 8, 13, 21, 34]
 
 # yield vs &block
 # def transaction_statement
